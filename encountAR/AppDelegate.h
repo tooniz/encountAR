39b980b6456d7316dd64e2b41a7fa1942ce5eef8
Ming Zhou <tooniz@Mings-MacBook-Pro.local>
2012-02-26 15:53:42 -0500

    Initial Commit

---
+++ encountAR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  encountAR
+//
+//  Created by Ming Zhou on 12-02-26.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
